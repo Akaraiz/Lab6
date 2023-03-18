4b0ab7dec253b273afe1773e96e9b129a662baa2
mixail <araiz5000@gmail.com>
2023-03-18 11:04:22 +0300

    Lab6 android

---
+++ app/src/main/java/com/example/lab6/TransitionActivity.java
@@ -0,0 +1,51 @@
+package com.example.lab6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.drawable.TransitionDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class TransitionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transition);
+        ImageWork();
+
+
+    }
+
+    public void ImageWork(){
+        ImageView imageView = findViewById(R.id.transition_image);
+        final TransitionDrawable transitionDrawable = (TransitionDrawable) imageView.getDrawable();
+
+        Button startTransition = findViewById(R.id.start_transition);
+        Button reverseTransition = findViewById(R.id.reverse_transition);
+        startTransition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                transitionDrawable.startTransition(1000);
+            }
+        });
+
+        reverseTransition.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                transitionDrawable.reverseTransition(1000);
+            }
+        });
+    }
+
+    public void Brows(View v){
+        WebView browser = (WebView) findViewById(R.id.webview);
+        browser.loadUrl("https://github.com/HeapGeap");
+    }
+}
